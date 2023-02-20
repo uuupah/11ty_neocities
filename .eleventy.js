80ee80e014f74a3793700e572bc56995a4ecf3fc
uuupah <uuupah@gmail.com>
2023-02-21 00:02:35 +1030

    add boilerplate

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/fonts");
+  eleventyConfig.addPassthroughCopy("./src/js");
+
+  return {
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      output: "public",
+      includes: "_includes",
+    },
+  };
+};
