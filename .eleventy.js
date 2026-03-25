30f4e24306e95ccd37df229497402a69041fde74
uuu <uuu@uuu.soy>
2026-03-25 21:08:46 +1030

    update eleventy drafts system

--- .eleventy.js
+++ .eleventy.js
@@ -172,39 +172,9 @@ module.exports = async function (eleventyConfig) {
     return new cleancss({}).minify(code).styles;
   });
 
-  // // the below three configs allow for excluding files from builds using draft: true
-  // // https://www.11ty.dev/docs/quicktips/draft-posts/
-  // When `permalink` is false, the file is not written to disk
-  eleventyConfig.addGlobalData("eleventyComputed.permalink", function () {
-    return (data) => {
-      // Always skip during non-watch/serve builds
-      if (data.draft && !process.env.BUILD_DRAFTS) {
-        return false;
-      }
-
-      return data.permalink;
-    };
-  });
-
-  // When `eleventyExcludeFromCollections` is true, the file is not included in any collections
-  eleventyConfig.addGlobalData(
-    "eleventyComputed.eleventyExcludeFromCollections",
-    function () {
-      return (data) => {
-        // Always exclude from non-watch/serve builds
-        if (data.draft && !process.env.BUILD_DRAFTS) {
-          return true;
-        }
-
-        return data.eleventyExcludeFromCollections;
-      };
-    },
-  );
-
-  eleventyConfig.on("eleventy.before", ({ runMode }) => {
-    // Set the environment variable
-    if (runMode === "serve" || runMode === "watch") {
-      process.env.BUILD_DRAFTS = true;
+  eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
+    if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
+      return false;
     }
   });
 
